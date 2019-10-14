1e272de6495c1fe50c998cf14b4a6bf451ddce37
LC.pi <jonylee.cn@gmail.com>
2019-10-14 16:01:42 +0800

    update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,12 @@
 sudo apt-get install vim -y
 sudo cp ./sources.list /etc/apt/sources.list
 sudo cp ./raspi.list /etc/apt/sources.list.d/raspi.list
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo "deb [arch=armhf] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo gpasswd -a $USER docker
+newgrp docker
